3ef3a434828c02974899b5dbc9803b9900b0577b
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-06 01:32:53 +0530

    Update Reducefunction.js

--- Reducefunction.js
+++ Reducefunction.js
@@ -5,3 +5,21 @@
      return res+el;
 });
  console.log(finalval);
+
+// reduce function me se Maximum value find karna::
+let  arr2 = [1,2,3,45,6,7,899,10,11,12,2];
+ let max = -1;
+ for(let i=0;i<arr2.length;i++){
+  if(max < arr2[i]){
+      max = arr2[i];
+    }
+ }
+ console.log(max);
+let max = arr2.reduce((max ,el) =>{
+    if(max <el){
+       return el;
+
+   }else{
+        return max;
+   }
+ });
