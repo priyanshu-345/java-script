6d7d37278054dcf1dd92dcef0065bd05e7a5b6cd
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-06 01:28:32 +0530

    Create Every and some.js
    now the Every and Some function in the java script

---
+++ Every and some.js
@@ -0,0 +1,7 @@
+//Every function::
+[2,3,45,667,88,].every((el)=> el%2 == 0);//retrun false
+[2,4,664,88,].every((el)=> el%2 == 0);//return true
+
+//some function::
+
+[2,3,45,667,88,].some((el)=> el%2 == 0);
