43aa7860e5f72b77328325c8e8cb2f95d4f6f0e6
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-06 01:56:37 +0530

    Create while loop and break.js
    while loop and break keyword

---
+++ while loop and break.js
@@ -0,0 +1,33 @@
+//while loop +++::
+let a="while loop";
+console.log(a);
+let i=1;
+while(i<=20){
+    console.log(i,"pranshu");
+    i++;
+}
+//while loop in ---
+let b="while loop";
+console.log(b);
+let j=1;
+while(j<=20){
+   console.log(j,"pranshu");
+    i++;
+}
+let c="even numbers";
+console.log(c);
+let k=1;
+while(k<=20){
+//    console.log(k,"pranshu");
+   i=i+2;
+}
+
+//break keyword:
+let g=1;
+while(g<=10){
+    if(g==8){
+        break
+    }
+    console.log(g);
+    g++;
+}
