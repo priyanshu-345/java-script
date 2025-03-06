dd47c509a2a7e6ad2cc4ca9a92d7b8fca28f81fc
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-06 23:31:03 +0530

    Create if and else statement.js
    if and else ,else if statement and a new trafice light question in java script

---
+++ if and else statement.js
@@ -0,0 +1,74 @@
+//if statement
+console.log("before ny if statement" );
+let age = 34;
+if (age>=23){
+  console.log("you can vote");
+  console.log("you can drive");
+  let g=4;
+  console.log(g*4);
+} 
+console.log("after ny if statement");
+//lets
+let firstname ="pranshu";
+if(firstname == "pranshu"){
+    console.log("yes its me")
+}
+//else if statement;
+let ageee=13;
+if(ageee>=18){
+    console.log("you can vote");
+} else if(ageee>=18){
+    console.log("yes you can vote");
+}
+else if(ageee<18){
+    console.log("yes you can vote");
+}
+//else if
+let mark = 67;
+if(mark>=80){
+    console.log("a");
+}
+ else if(mark>=60){
+    console.log("b");
+ }
+ else if(mark>=50){
+    console.log("c");
+ }
+ else if(mark>=60){
+    console.log("f");
+ }
+
+ //else if
+
+ let month = 'may';
+ if(month==="january"){
+    console.log("winter is here");
+ }
+ else if(month==="may"){
+    console.log("summer is here");
+ }
+
+ //else statement
+
+ let ageeee =18;
+ if(ageeee>=18){
+    console.log("yes  i want fuck you");
+ } else{
+    console.log("you cannot vote");
+ }
+
+ //new trafice light
+ let color='white';
+//trafic light system
+if(color==='red'){
+    console.log("stop! light color is red");
+}
+ else if(color==='yellow'){
+    console.log("slow down light color is yellow");
+}
+else if(color==='green'){
+    console.log("go now light color is green");
+}
+else{
+    console.log("traffic light is broken");
+}
