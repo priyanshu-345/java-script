f778d9e55d7e68107f005212a2fabb763d7a9163
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-06 23:28:37 +0530

    Create normal operation.js
    assingment operation camparison operation if statement

---
+++ normal operation.js
@@ -0,0 +1,45 @@
+console.log("hello pranshu")  
+console.log("shukla ji");
+let a = 67;
+let b = 90;
+console.log("sum is : ", a+ b);
+// question//
+let penprice = 90;
+let milkprice = 35;
+//let output = "the total price is : " + (penprice + milkprice) + " rupees"
+let output = ` the total price is :  ${penprice + milkprice} + rupees,`;
+console.log(output); 
+
+//arithmetic operators
+let q = 67;
+let w = 78;
+console.log(q+w);
+console.log(q-w);
+console.log(q%w);
+console.log(q/w);
+console.log(q*w);
+console.log(q**w);
+//unary operators
+console.log(++q);
+console.log(w++);
+
+//assingment operators:
+let f = 90;
+let i = 89;
+console.log(f*i); 
+
+//camparison operators
+let agee=17;
+console.log(agee>=18);
+"1234"===1234;//false
+1==='1'//false  
+//if statement
+console.log("before ny if statement" );
+let age = 34;
+if (age>=23){
+  console.log("you can vote");
+  console.log("you can drive");
+  let g=4;
+  console.log(g*4);
+} 
+console.log("after ny if statement");
