8c6631c299508176e3e8f64b7e2a2bb289c29aa8
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-07 19:06:06 +0530

    Create normal calculator.js
    normal calculator in java script

---
+++ normal calculator.js
@@ -0,0 +1,30 @@
+//normal calculator::
+const calculator= {
+    num: 78,
+    add: function(a, b){
+        return a+b;
+    },
+    sub: function(a, b){
+        return a-b;
+    },
+    mul: function(a, b){
+        return a*b;
+    },
+    div: function(a, b){
+        return a/b;
+    },
+    exp: function(a, b){
+        return a%b;
+    },
+    power: function(a, b){
+        return a**b;
+    },
+}
+const cal = {
+    add(a,b){
+        return a+ b;
+    },
+    sub(a,b){
+        return a - b;
+    }
+};
