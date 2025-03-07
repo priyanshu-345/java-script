271548918e7f5012069158733cdc5a426b392762
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-07 20:46:21 +0530

    Create Alert and prompt or concatination function.js

---
+++ Alert and prompt or concatination function.js
@@ -0,0 +1,28 @@
+//Alert & Prompt
+ //alert("something  is wrong");//pop up text
+ //console.log("this is a simple log");
+ //console.warn("this is wrong password");
+
+//Prompt
+ //let firstname = prompt ("enter your name");
+//console.log(firstname);
+
+//concatination
+
+//let Firstname  = prompt("enter your first name");
+//let Lastname  = prompt("enter your last name");
+//let msg = "Welcome  " +  Firstname+ " "  +  Lastname  +   "!";
+//alert(msg);
+
+//trim and remove messages
+//let mes="    pran  shu    ";
+//console.log(mes);
+let password = prompt("set your password");
+let newpass = password.trim();//trim ka use spaces ko hatane me kiya jata hai
+console.log(password);//if newpass write karte hai to space nahi hoga
+//upper and lower case
+let name= "pranshu shukkla";
+let meg="error";
+console.log(meg.toUpperCase());
+//indexof:
+let mes = "IloveSidhu";
