d14299ca27ae3f23ed692fac57ced6f8ab694575
Priyanshu shukla <priyanshushukla9006@gmail.com>
2025-03-07 20:55:11 +0530

    Create uppercase,slice,replace,method channing,object literals.js
    uppercase, slice, replace, method channing , object literals in java script

---
+++ uppercase,slice,replace,method channing,object literals.js
@@ -0,0 +1,55 @@
+//upper and lower case::
+
+let name= "pranshu shukkla";
+let meg="error";
+console.log(meg.toUpperCase());
+//indexof:
+let mes = "IloveSidhu";
+
+//methods channing
+
+let msg="             sidhu                    ";
+let newm=msg.trim();
+console.log("after trim:",newm);
+newm=newm.toUpperCase();
+console.log("after uppercase:",newm);
+let n=msg.trim().toUpperCase();//direct uppercase and trim also:
+console.log(n);
+
+//Slice 
+let m="Sidhu mosse wala";
+console.log(m.slice(0,8)); 
+
+//Repalce::
+let i=" I am fully Mad";
+//trim and uppercase::
+
+ let g="pranshu";
+ console.log(g.trim().toUpperCase());  
+ let f="pranshushukla";
+ 
+ //object literals::
+let car = {
+    names : "parnshu shukla",
+    age : 45,
+    carname: "fortuner",
+    marks:90  
+}
+let car2 = ["pranshu",23,89,"bmw"];
+const namess={
+    brand:"pranshushukla",
+    age :56,
+    values:"AA"
+};
+const item = {
+    price: 89.90,
+    discount : 56,
+    color : ["red","yellow"]
+};
+const post = {
+    username:"pranshushukla@9005",
+    content:"this is my @firstpost",
+    likes: 5000,
+    reposts:45,
+    tags:["@pranshushukla","@engineer's"]
+};
